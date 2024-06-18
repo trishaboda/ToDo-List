f701a46ddb3c47e1741cc512e85c338d77c53db8
Trisha Boda <141979558+trishaboda@users.noreply.github.com>
2024-06-18 11:02:13 +0530

    app components ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  taskArray = [{taskName: "Brush teeth", isCompleted: false}];
   title = 'angularApp';
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value, 
+      isCompleted: false,
+    });
+      // clear placeholder after submitting
+      form.reset();
+}
+  onDelete(index: number) {
+    console.log(index);
+    this.taskArray.splice(index, 1);
+  }
+  onCheck(index: number){
+    console.log(this.taskArray);
+    //change value of boolean to opposite
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
